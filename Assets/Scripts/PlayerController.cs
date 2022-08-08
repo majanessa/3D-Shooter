279dca1d5402565c7ecd270e97fcb66ee0dfbbbb
majanessa <majanessajane@gmail.com>
2022-08-08 17:19:47 +0300

    Add decoration, audio and ps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rb;
     private PhotonView _photonView;
-    public float speed = 5f, rotateSpeed = 4f;
+    public float speed = 5f, rotateSpeed = 4f, range = 100f, power = 10f, radius = 5f;
     public int _attackDamage = 25;
     private int _health = 100;
-
+    public ParticleSystem shootEffect, explosionEffect;
+    public AudioSource runningAudio, shootAudio;
+    
     private void Awake()
     {
         _photonView = GetComponent<PhotonView>();
@@ -41,15 +43,27 @@ public class PlayerController : MonoBehaviour
 
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1"))
         {
+            shootEffect.Play();
+            shootAudio.Play();
             Camera cam = transform.GetChild(0)?.GetComponent<Camera>();
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            RaycastHit hit;
+            Vector3 shootPos = new Vector3(cam.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+            if (Physics.Raycast(shootPos, cam.transform.forward, out hit, range))
             {
-                if (hit.collider.CompareTag("Player"))
-                    hit.collider.GetComponent<PlayerController>().Damage(_attackDamage);
+                Debug.Log("Объект: " + hit.transform.gameObject.name);
+                if (hit.collider.CompareTag("Player")) {
+                     hit.collider.GetComponent<PlayerController>().Damage(_attackDamage);
+                     hit.collider.GetComponent<Rigidbody>().AddForce(new Vector3(0, 100f, 0));
+                }
+
+                if (hit.rigidbody != null) {
+                    Vector3 pos = hit.collider.GetComponent<Transform>().position;
+                    ParticleSystem explode = Instantiate(explosionEffect, pos, Quaternion.identity);
+
+                    Destroy(explode.transform.gameObject, 0.3f);
+                }
             }
         }
     }
@@ -77,6 +91,11 @@ public class PlayerController : MonoBehaviour
 
     private void MovePlayer()
     {
-        _rb.MovePosition(transform.position + (transform.forward * Time.fixedDeltaTime * speed * Input.GetAxis("Vertical")));
+        float vertMove = Input.GetAxis("Vertical");
+        _rb.MovePosition(transform.position + (transform.forward * Time.fixedDeltaTime * speed * vertMove));
+        if ((vertMove > 0 || vertMove < 0) && !runningAudio.isPlaying)
+            runningAudio.Play();
+        else if (vertMove == 0 && runningAudio.isPlaying)
+            runningAudio.Stop();
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,6 +7,14 @@ using Photon.Pun;
 public class PlayerManager : MonoBehaviour
 {
     private PhotonView _photonView;
+    private float[,] coordinates = {
+        {-1.4f, -35.24f},
+        {8.2f, -5.1f},
+        {37.8f, -5.1f},
+        {29.4f, 10.09f},
+        {-8.4f, -28.7f}
+    };
+
     private void Start()
     {
         _photonView = GetComponent<PhotonView>();
@@ -16,6 +24,9 @@ public class PlayerManager : MonoBehaviour
 
     private void CreatePlayer()
     {
-        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+        int randomIndex = UnityEngine.Random.Range(0, coordinates.Length - 1);
+
+        Vector3 playerPosition = new Vector3(coordinates[randomIndex, 0], 0, coordinates[randomIndex, 1]);
+        PhotonNetwork.Instantiate("Player", playerPosition, Quaternion.identity);
     }
 }
